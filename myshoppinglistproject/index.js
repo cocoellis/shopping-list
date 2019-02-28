31949f62b313dc15a3df0d7af39b1211e25b98f5
cocoellis <38797064+cocoellis@users.noreply.github.com>
2019-02-28 07:45:44 -0600

    Add files via upload

--- myshoppinglistproject/index.js
+++ myshoppinglistproject/index.js
@@ -1,135 +1,44 @@
-//     <form id="js-shopping-list-form">
-//       <label for="shopping-list-entry">Add an item</label>
-//       <input type="text" name="shopping-list-entry" id="shopping-list-entry" placeholder="e.g., broccoli">
-//       <button type="submit">Add item</button>
-//     <ul class="shopping-list">
-//       <li>
-//         <span class="shopping-item">apples</span>
-//         <div class="shopping-item-controls">
-//           <button class="shopping-item-toggle">
-//             <span class="button-label">check</span>
-//           </button>
-//           <button class="shopping-item-delete">
-//             <span class="button-label">delete</span>
-//           </button>
-//         </div>
-//       </li>
-
-  $(function() {
-    $('#js-shopping-list-form').submit(event => {
-      event.preventDefault();
-      let newItem = [];
-      newItem.push($('#shopping-list-entry'));
-      $("ul").append("<li>" + newItem + "</li>")
-    }
-  )
-  $('.shopping-item-delete').click(function() {
-    $("#shopping-list").closest('li').remove();
+$( document ).ready(function() {
+  //jQuery call for listent for every click of the submit button
+      $('#js-shopping-list-form').submit(event => {
+  //Stops any default functions preformed by the form element
+          event.preventDefault();
+  //Creates a variable to hold the input item
+          const entry = $(this).find(
+              'input[name="shopping-list-entry"]').val();
+  //Creats a variable to hold the new items div
+          const addItem = [];
+  //If Statment that adds the new item only if input is something
+          if (entry == '') {
+              alert ("Must be a valid shopping item.");
+              return false;
+          }
+          else {
+              addItem.push($(
+                  `<li> 
+                  <span class='shopping-item'>${entry}</span>
+                  <div class="shopping-item-controls">
+                  <button class="shopping-item-toggle">
+                  <span class="button-label">check</span>
+                  </button>
+                  <button class="shopping-item-delete">
+                  <span class="button-label">delete</span>
+                  </button>
+                  </div>
+                  </li>`    
+              ));
+          }
+  //Updates the DOM with new item
+          $("ul").append(addItem);
+  //Resets the input box to clear
+          $('input[type="text"], textarea').val('');
+      });
+  //When the checked button is clicked, it toggels between checked and unchecked
+      $('.shopping-list').on('click', '.shopping-item-toggle', function(){
+          $(this).closest('li').children().first().toggleClass("shopping-item__checked");
+      });
+  //When the delete button is clicked, it removes the item
+      $('.shopping-list').on('click', '.shopping-item-delete',function(){
+          $(this).closest('li').remove();
+      })  
   });
-
-  $(".shopping-item-toggle").click(function(){
-    $("#shopping-list").closest('li').toggle();
-  });
-})
-
-
-//   function getItem(state, itemIndex) {
-//     return state.items[itemIndex];
-//   }
-  
-//   function deleteItem(state, itemIndex) {
-//     state.items.splice(itemIndex, 1);
-//   }
-  
-//   function updateItem(state, itemIndex, newItemState) {
-//     state.items[itemIndex] = newItemState;
-//   }
-  
-//   // DOM manipulation
-  
-//   function renderItem(item, itemId, itemTemplate, itemDataAttr) {
-//     var element = $(itemTemplate);
-//     element.find('.js-shopping-item').text(item.displayName);
-//     if (item.checkedOff) {
-//       element.find('.js-shopping-item').addClass('shopping-item__checked');
-//     }
-//     element.find('.js-shopping-item-toggle')
-//     element.attr(itemDataAttr, itemId);
-//     return element;
-//   }
-  
-//   function renderList(state, listElement, itemDataAttr) {
-//     var itemsHTML = state.items.map(
-//       function(item, index) {
-//         return renderItem(item, index, listItemTemplate, itemDataAttr);
-//     });
-//     listElement.html(itemsHTML);
-//   }
-  
-  
-//   // Event listeners
-  
-//   function handleItemAdds(
-//     formElement, newItemIdentifier, itemDataAttr, listElement, state) {
-  
-//     formElement.submit(function(event) {
-//       event.preventDefault();
-//       var newItem = formElement.find(newItemIdentifier).val();
-//       addItem(state, newItem);
-//       renderList(state, listElement, itemDataAttr);
-//       // reset form
-//       this.reset();
-//     });
-//   }
-  
-//   function handleItemDeletes(
-//     formElement, removeIdentifier, itemDataAttr, listElement, state) {
-  
-//     listElement.on('click', removeIdentifier, function(event) {
-//       var itemIndex = parseInt($(this).closest('li').attr(itemDataAttr));
-//       deleteItem(state, itemIndex);
-//       renderList(state, listElement, itemDataAttr);
-//     })
-//   }
-  
-  
-//   function handleItemToggles(
-//     listElement, toggleIdentifier, itemDataAttr, state) {
-  
-//     listElement.on('click', toggleIdentifier, function(event) {
-//       var itemId = $(event.currentTarget.closest('li')).attr(itemDataAttr);
-//       var oldItem = getItem(state, itemId);
-  
-//       updateItem(state, itemId, {
-//         displayName: oldItem.displayName,
-//         checkedOff: !oldItem.checkedOff
-//       });
-//       renderList(state, listElement, itemDataAttr)
-//     });
-//   }
-  
-  
-//   $(function() {
-//     var formElement = $('#js-shopping-list-form');
-//     var listElement = $('.js-shopping-list');
-  
-//     // from index.html -- it's the id of the input
-//     // containing shopping list items
-//     var newItemIdentifier = '#js-new-item';
-  
-//     // from `listItemTemplate` at top of this file. for each
-//     // displayed shopping list item, we'll be adding a button
-//     // that has this class name on it
-//     var removeIdentifier = '.js-shopping-item-delete';
-  
-//     // we'll use this attribute to store the id of the list item
-//     var itemDataAttr = 'data-list-item-id';
-  
-//     //
-//     var toggleIdentifier = '.js-shopping-item-toggle'
-  
-//     handleItemAdds(
-//       formElement, newItemIdentifier, itemDataAttr, listElement, state);
-//     handleItemDeletes(
-//       formElement, removeIdentifier, itemDataAttr, listElement, state);
-//     handleItemToggles(listElement, toggleIdentifier, itemDataAttr, state);
